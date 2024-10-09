8a05b2f0fe15f74012b0642b7008a2dd7b5c60a7
Natalia Tabunkina <nataliatabunkina@gmail.com>
2024-10-09 18:54:15 +0300

    Changed places of figuries. Pawns replaced by checkers

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -22,32 +22,39 @@ public class ChessBoard {
     }
 
     private void setupPieces() {
-        // Place Rooks
-        board[0][0] = new Rook(PieceColor.BLACK, new Position(0, 0));
-        board[0][7] = new Rook(PieceColor.BLACK, new Position(0, 7));
-        board[7][0] = new Rook(PieceColor.WHITE, new Position(7, 0));
-        board[7][7] = new Rook(PieceColor.WHITE, new Position(7, 7));
+       /* // Place Rooks
+        //board[0][0] = new Pawn(PieceColor.BLACK, new Position(0, 0));
+        board[0][7] = new Pawn(PieceColor.BLACK, new Position(0, 7));
+        board[7][0] = new Pawn(PieceColor.WHITE, new Position(7, 0));
+        //board[7][7] = new Rook(PieceColor.WHITE, new Position(7, 7));
         // Place Knights
-        board[0][1] = new Knight(PieceColor.BLACK, new Position(0, 1));
-        board[0][6] = new Knight(PieceColor.BLACK, new Position(0, 6));
-        board[7][1] = new Knight(PieceColor.WHITE, new Position(7, 1));
-        board[7][6] = new Knight(PieceColor.WHITE, new Position(7, 6));
+        //board[0][1] = new Knight(PieceColor.BLACK, new Position(0, 1));
+        board[0][6] = new Pawn(PieceColor.BLACK, new Position(0, 6));
+        board[7][1] = new Pawn(PieceColor.WHITE, new Position(7, 1));
+        //board[7][6] = new Knight(PieceColor.WHITE, new Position(7, 6));
         // Place Bishops
-        board[0][2] = new Bishop(PieceColor.BLACK, new Position(0, 2));
-        board[0][5] = new Bishop(PieceColor.BLACK, new Position(0, 5));
-        board[7][2] = new Bishop(PieceColor.WHITE, new Position(7, 2));
-        board[7][5] = new Bishop(PieceColor.WHITE, new Position(7, 5));
+        //board[0][2] = new Bishop(PieceColor.BLACK, new Position(0, 2));
+        board[0][5] = new Pawn(PieceColor.BLACK, new Position(0, 5));
+        board[7][2] = new Pawn(PieceColor.WHITE, new Position(7, 2));
+        //board[7][5] = new Bishop(PieceColor.WHITE, new Position(7, 5));
         // Place Queens
-        board[0][3] = new Queen(PieceColor.BLACK, new Position(0, 3));
-        board[7][3] = new Queen(PieceColor.WHITE, new Position(7, 3));
+        //board[0][3] = new Queen(PieceColor.BLACK, new Position(0, 3));
+        //board[7][3] = new Queen(PieceColor.WHITE, new Position(7, 3));
         // Place Kings
-        board[0][4] = new King(PieceColor.BLACK, new Position(0, 4));
-        board[7][4] = new King(PieceColor.WHITE, new Position(7, 4));
+        //board[0][4] = new King(PieceColor.BLACK, new Position(0, 4));
+        //board[7][4] = new King(PieceColor.WHITE, new Position(7, 4));*/
         // Place Pawns
-        for (int i = 0; i < 8; i++) {
+        for (int i = 5; i < 8; i++) {
+            board[0][i] = new Pawn(PieceColor.BLACK, new Position(0, i));
             board[1][i] = new Pawn(PieceColor.BLACK, new Position(1, i));
-            board[6][i] = new Pawn(PieceColor.WHITE, new Position(6, i));
+            board[2][i] = new Pawn(PieceColor.BLACK, new Position(2, i));
         }
+        for (int i = 0; i < 3; i++) {
+            board[5][i] = new Pawn(PieceColor.WHITE, new Position(5, i));
+            board[6][i] = new Pawn(PieceColor.WHITE, new Position(6, i));
+            board[7][i] = new Pawn(PieceColor.WHITE, new Position(7, i));
+        }
+
     }
 
     public void movePiece(Position start, Position end) {
